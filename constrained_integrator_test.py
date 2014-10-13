b97bdb7dff5989e4cc01d6d99adfe2cf08ac0515
Steven Delong <steven.d.delong@gmail.com>
2014-10-13 15:49:23 -0400

    Test for normal vector.  Added projection matrix.

--- constrained_integrator_test.py
+++ constrained_integrator_test.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 from constrained_integrator import ConstrainedIntegrator
 
 class TestConstrainedIntegrator(unittest.TestCase):
@@ -12,9 +13,10 @@ class TestConstrainedIntegrator(unittest.TestCase):
   def test_initialize(self):
     """ Test that the integrator is set up correctly. """
     scheme = "RFD"
+    initial_position = [0.0, 0.0]
     
     test_integrator = ConstrainedIntegrator(
-      self.empty_constraint, self.mobility, scheme)
+      self.empty_constraint, self.mobility, scheme, initial_position)
     # Test dimensions
     self.assertEqual(test_integrator.dim, 2)
     # Test Mobility
@@ -30,10 +32,26 @@ class TestConstrainedIntegrator(unittest.TestCase):
   def test_scheme_check(self):
     """ Test that a nonexistant scheme isn't accepted. """
     scheme = "DOESNTEXIST"
+    initial_position = [0.0, 0.0]
     self.assertRaises(
       NotImplementedError,
       ConstrainedIntegrator,
-      self.empty_constraint, self.mobility, scheme)
+      self.empty_constraint, self.mobility, scheme, initial_position)
+    
+  def test_normal_vector(self):
+    """ Test that normal vector points in the right direction """
+    scheme = "RFD"
+    initial_position = [1.2, 0.0]
+    def sphere_constraint(x):
+      return np.sqrt(x[0]*x[0] + x[1]*x[1]) - 1.2
+    
+    test_integrator = ConstrainedIntegrator(
+      sphere_constraint, self.mobility, scheme, initial_position)
+    
+    normal_vector = test_integrator.NormalVector()
+    self.assertEqual(normal_vector[0], 1.0)
+    self.assertEqual(normal_vector[1], 0.0)
+
 
 if __name__ == "__main__":
   unittest.main()
