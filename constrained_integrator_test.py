950426c0af521d43e739dc4f30fcca05f8590d2d
skwaap <steven.d.delong@gmail.com>
2014-10-13 22:01:26 -0400

    Added test for projection matrix. Switched to using numpy matrices.

--- constrained_integrator_test.py
+++ constrained_integrator_test.py
@@ -5,7 +5,7 @@ from constrained_integrator import ConstrainedIntegrator
 class TestConstrainedIntegrator(unittest.TestCase):
 
   def setUp(self):
-    self.mobility = [[1.0, 0.0], [0.0, 1.0]]
+    self.mobility = np.matrix([[1.0, 0.0], [0.0, 1.0]])
     
   def empty_constraint(self, x):
     return 0.0
@@ -13,17 +13,17 @@ class TestConstrainedIntegrator(unittest.TestCase):
   def test_initialize(self):
     """ Test that the integrator is set up correctly. """
     scheme = "RFD"
-    initial_position = [0.0, 0.0]
+    initial_position = np.matrix([[0.0], [0.0]])
     
     test_integrator = ConstrainedIntegrator(
       self.empty_constraint, self.mobility, scheme, initial_position)
     # Test dimensions
     self.assertEqual(test_integrator.dim, 2)
     # Test Mobility
-    self.assertEqual(test_integrator.mobility[0][0], 1.0)
-    self.assertEqual(test_integrator.mobility[1][0], 0.0)
-    self.assertEqual(test_integrator.mobility[0][1], 0.0)
-    self.assertEqual(test_integrator.mobility[1][1], 1.0)
+    self.assertEqual(test_integrator.mobility[0,0], 1.0)
+    self.assertEqual(test_integrator.mobility[1,0], 0.0)
+    self.assertEqual(test_integrator.mobility[0,1], 0.0)
+    self.assertEqual(test_integrator.mobility[1,1], 1.0)
     # Test constraint.
     self.assertEqual(test_integrator.surface_function(10.0), 0.0)
     # Test scheme.
@@ -32,7 +32,7 @@ class TestConstrainedIntegrator(unittest.TestCase):
   def test_scheme_check(self):
     """ Test that a nonexistant scheme isn't accepted. """
     scheme = "DOESNTEXIST"
-    initial_position = [0.0, 0.0]
+    initial_position = np.matrix([[0.0], [0.0]])
     self.assertRaises(
       NotImplementedError,
       ConstrainedIntegrator,
@@ -40,18 +40,33 @@ class TestConstrainedIntegrator(unittest.TestCase):
     
   def test_normal_vector(self):
     """ Test that normal vector points in the right direction """
-    scheme = "RFD"
-    initial_position = [1.2, 0.0]
+    scheme = 'RFD'
+    initial_position = np.matrix([[1.2], [0.0]])
     def sphere_constraint(x):
-      return np.sqrt(x[0]*x[0] + x[1]*x[1]) - 1.2
+      return np.sqrt(x[0,0]*x[0,0] + x[1,0]*x[1,0]) - 1.2
     
     test_integrator = ConstrainedIntegrator(
       sphere_constraint, self.mobility, scheme, initial_position)
     
-    normal_vector = test_integrator.NormalVector()
-    self.assertEqual(normal_vector[0], 1.0)
-    self.assertEqual(normal_vector[1], 0.0)
+    normal_vector = test_integrator.NormalVector(initial_position)
+    self.assertEqual(normal_vector[0,0], 1.0)
+    self.assertEqual(normal_vector[1,0], 0.0)
+    self.assertEqual(normal_vector.shape, (2, 1))
 
+  def test_projection_matrix(self):
+    scheme = 'RFD'
+    initial_position = np.matrix([[1.2], [0.0]])
+    def sphere_constraint(x):
+      return np.sqrt(x[0,0]*x[0,0] + x[1,0]*x[1,0]) - 1.2
+
+    test_integrator = ConstrainedIntegrator(
+      sphere_constraint, self.mobility, scheme, initial_position)
+
+    projection_vector = test_integrator.ProjectionMatrix(initial_position)
+    self.assertEqual(projection_vector[0,0], 0.0)
+    self.assertEqual(projection_vector[0,1], 0.0)
+    self.assertEqual(projection_vector[1,0], 0.0)
+    self.assertEqual(projection_vector[1,1], 1.0)
 
 if __name__ == "__main__":
   unittest.main()
