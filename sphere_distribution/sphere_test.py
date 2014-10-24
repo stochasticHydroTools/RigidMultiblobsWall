84381357eb408f9650b0a330b2cc719f3260d875
skwaap <steven.d.delong@gmail.com>
2014-10-24 09:39:17 -0400

    Added sphere test file.

---
+++ sphere_distribution/sphere_test.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def GenerateRandomRotationMatrix():
+  ''' 
+  Generate a rotation matrix that represents a uniformly
+  random distributed rotation.
+  '''
+  A = np.matrix([np.random.normal(0., 1., 3) for _ in range(3)])
+  P = np.linalg.cholesky(A.T*A)
+  R = A*np.linalg.inv(P)
+
+  return R
+
+def MatrixToQuaterion():
+  ''' Convert rotation matrix to a quaternion '''
+
+
+  
+
+  
