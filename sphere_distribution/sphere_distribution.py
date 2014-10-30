Fixed rotation to quaternion to use negative signs, too.

sphere_distribution/sphere_distribution.py
@@ -13,13 +13,34 @@ PROFILE = 0
 def GenerateRandomRotationMatrix():
   ''' 
   Generate a rotation matrix that represents a uniformly
-  random distributed rotation.
+  random distributed rotation.  This uses polar decomposition.
   '''
   A = np.matrix([np.random.normal(0., 1., 3) for _ in range(3)])
   U, sigma, V = np.linalg.svd(A)
   R = U*V.T
   return R
 
+def GenerateRandomRotationMatrixManual():
+  ''' 
+  Generate a random rotation matrix uniformly by choosing
+  one axis at a time. 
+  '''
+  # Generate x, uniformly on the sphere.
+  x = np.random.normal(0., 1., 3)
+  x = x/np.linalg.norm(x)
+  
+  # Generate y, uniformly on the circle around x.
+  p = np.random.normal(0., 1., 3)
+  p = p/np.linalg.norm(p)
+  y = np.cross(x, p)
+  y = y/np.linalg.norm(y)
+  
+  # Generate z as x cross y.
+  z = np.cross(x, y)
+  z = z/np.linalg.norm(z)
+  return np.matrix([x, y, z])
+
+  
 def MatrixToQuaternion(R):
   ''' 
   Convert rotation matrix to a quaternion. 
@@ -42,6 +63,10 @@ def MatrixToQuaternion(R):
   
   # Back out values of s and p.
   s = np.sqrt(s_squared)
+  # + and - are the same rotation, pick a sign randomly 
+  # to test against the uniform distribution.
+  if np.random.randint(2) == 0:
+    s = -1.*s
   p1 = p1_over_s*s
   p2 = p2_over_s*s
   p3 = p3_over_s*s
@@ -56,13 +81,18 @@ if __name__ == "__main__":
     pr.enable()
 
   samples = []
+  uniform_samples = []
   for k in range(int(sys.argv[1])):
     samples.append(MatrixToQuaternion(GenerateRandomRotationMatrix()))
+    x = np.random.normal(0., 1., 4)
+    x = x/np.linalg.norm(x)
+    uniform_samples.append(x)
   
   # Analyze distribution on 3-sphere
-  uniform_analyzer =ua.UniformAnalyzer(samples)
-  uniform_analyzer.AnalyzeSamples()
-    
+  rotation_analyzer = ua.UniformAnalyzer(samples, 'Rotations')
+  uniform_analyzer = ua.UniformAnalyzer(uniform_samples, 'Uniform')
+  ua.CompareDistributions([rotation_analyzer, uniform_analyzer])
+      
   if PROFILE:
     pr.disable()
     s = StringIO.StringIO()

sphere_distribution/sphere_distribution_test.py
@@ -8,7 +8,6 @@ class TestSphereDistribution(unittest.TestCase):
   def setUp(self):
     pass
 
-
   def test_generate_random_matrix(self):
     ''' Test that the matrix generated is a rotation. '''
 
@@ -23,6 +22,22 @@ class TestSphereDistribution(unittest.TestCase):
     self.assertAlmostEqual(np.inner(A[0], A[1]), 0.)
     self.assertAlmostEqual(np.inner(A[1], A[2]), 0.)
     self.assertAlmostEqual(np.inner(A[0], A[2]), 0.)
+
+  def test_generate_random_matrix_manual(self):
+    ''' Test that the matrix generated is a rotation. '''
+
+    A = sphere_distribution.GenerateRandomRotationMatrixManual()
+
+    # Check for unit norm.
+    self.assertAlmostEqual(np.linalg.norm(A[0]), 1.0)
+    self.assertAlmostEqual(np.linalg.norm(A[1]), 1.0)
+    self.assertAlmostEqual(np.linalg.norm(A[2]), 1.0)
+
+    # Check for orthogonality.
+    self.assertAlmostEqual(np.inner(A[0], A[1]), 0.)
+    self.assertAlmostEqual(np.inner(A[1], A[2]), 0.)
+    self.assertAlmostEqual(np.inner(A[0], A[2]), 0.)
+
 
   def test_matrix_to_quaternion(self):
     ''' Test that we get the right quaternion back from a rotation matrix '''
@@ -39,8 +54,8 @@ class TestSphereDistribution(unittest.TestCase):
                        [p1*p3 + s*p2, p2*p3 - s*p1, p3**2 + s**2 - 0.5]])
     # Get the Quaternion from the matrix.
     quaternion = sphere_distribution.MatrixToQuaternion(R)
-    # The + and - of a quaternion indicate the same rotation. We always choose
-    # s to be positive for no real reason.
+    # The + and - of a quaternion indicate the same rotation. We choose the sign 
+    # with 50% probability each.
     if (quaternion[0]/s) > 0:
       self.assertAlmostEqual(quaternion[0], s)
       self.assertAlmostEqual(quaternion[1], p1)

sphere_distribution/uniform_analyzer.py
@@ -3,13 +3,14 @@ Class to look at samples on a sphere and verify that they are a uniform
 distribution.
 '''
 import numpy as np
+from matplotlib import pyplot
 
 class UniformAnalyzer(object):
   ''' 
   This object just takes samples on a sphere and analyzes their 
   distribution to determine if they are uniformly distributed.
   '''
-  def __init__(self, samples):
+  def __init__(self, samples, name):
     ''' 
     Just copy the reference to the list of samples.
     Each sample should be of the same length, and represent
@@ -17,11 +18,14 @@ class UniformAnalyzer(object):
     '''
     self.samples = samples
     self.dim = len(self.samples[0])
+    # Name used for plotting, etc.
+    self.name = name
 
   def AnalyzeSamples(self):
     ''' Analyze samples by calculating means of spherical harmonics. '''
-    statistics = [[] for _ in range(10)]
-    n_xi_eta_pairs = 16
+    # Here 10 is the number of L's we will look at.
+    statistics = [[] for _ in range(10)]  
+    n_xi_eta_pairs = 2
     for k in range(n_xi_eta_pairs):
       xi, eta = self.GenerateXiEta()
       for L in range(1, len(statistics) + 1):
@@ -33,18 +37,21 @@ class UniformAnalyzer(object):
           theta = np.arctan(v/u)  + (u < 0)*np.pi
           harmonics.append(np.cos(L*theta))
         statistics[L-1].append(np.mean(harmonics))
+    L_means = []
     for L in range(1, len(statistics) + 1):
+      L_means.append(np.mean(statistics[L-1]))
       print ('Mean at L = %d is: %f +/- %f' % 
-             (L, np.mean(statistics[L-1]), 
+             (L, L_means[-1], 
               np.std(statistics[L-1])))
 
+    return L_means
 
   def GenerateXiEta(self):
     ''' Generate a random pair of orthonormal vectors. '''
-    xi = np.random.normal(0, 1, self.dim)
+    xi = np.random.normal(0., 1., self.dim)
     xi = xi/np.linalg.norm(xi)
     
-    eta = np.random.normal(0, 1, self.dim)
+    eta = np.random.normal(0., 1., self.dim)
     eta = eta - np.inner(eta, xi)*xi
     
     eta = eta/np.linalg.norm(eta)
@@ -52,6 +59,17 @@ class UniformAnalyzer(object):
     return xi, eta
     
     
-    
+def CompareDistributions(uniform_analyzer_list):
+  ''' 
+  Takes a list of UniformAnalyzer objects, and plots their average L values over
+  the xi eta pairs generated for each.
+  '''
+  for ua in uniform_analyzer_list:
+    pyplot.plot(ua.AnalyzeSamples(), label=ua.name)
+  
+  pyplot.legend(loc="best", prop={'size': 9})
+  pyplot.savefig("./CompareDistributions.pdf")
+  
+  
     
     