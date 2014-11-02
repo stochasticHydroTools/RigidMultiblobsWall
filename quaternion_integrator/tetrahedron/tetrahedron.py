f945211eba3771a6f80921edeb6c61218427fc23
skwaap <steven.d.delong@gmail.com>
2014-11-02 15:49:44 -0500

    initial commit of tetrahedron script.

---
+++ quaternion_integrator/tetrahedron/tetrahedron.py
@@ -0,0 +1,14 @@
+''' Script to test a tetrahedron near a wall '''
+
+import numpy as np
+from quaternion import Quaternion
+from quaternion_integrator import QuaternionIntegrator
+
+def TetrahedronMobility(self, position):
+  '''
+  Calculate the mobility, torque -> angular velocity, at position 
+  In this case, position is length 1, as there is just 1 quaternion.
+  The mobility is equal to 
+  '''
+  
+
