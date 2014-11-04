finished renaming, and fixed some issues in quaternion integrator.

quaternion_integrator/quaternion.py
@@ -8,12 +8,12 @@ class Quaternion(object):
   def __init__(self, entries):
     ''' Constructor, takes 4 entries = s, p1, p2, p3 as a numpy array. '''
     self.entries = entries
-    self.s = entries[0]
-    self.p = entries[1:4]
+    self.s = np.array(entries[0])
+    self.p = np.array(entries[1:4])
 
 
   @classmethod
-  def FromRotation(cls, phi):
+  def from_rotation(cls, phi):
     ''' Create a quaternion given an angle of rotation phi,
     which represents a rotation clockwise about the vector phi of magnitude 
     phi. This will be used with phi = omega*dt or similar in the integrator.'''
@@ -35,7 +35,7 @@ class Quaternion(object):
     return Quaternion(np.concatenate(([s], p)))
 
 
-  def RotationMatrix(self):
+  def rotation_matrix(self):
     ''' 
     Return the rotation matrix representing rotation
     by this quaternion.
@@ -49,16 +49,6 @@ class Quaternion(object):
             (self.s**2 - 0.5)*np.identity(3)
             - self.s*P)
     return R
-  
-  def GetP(self):
-    ''' Get the p vector, last 3 entries. '''
-    #TODO: Figure out how to use params decorator.
-    return self.entries[1:4]
 
-  
-  def GetS(self):
-    ''' Return scalar s, the first entry. '''
-    #TODO: Figure out how to use params decorator.
-    return self.entries[0]
-
-
+  def __str__(self):
+    return '[ %f, %f, %f, %f ]' % (self.s, self.p[0], self.p[1], self.p[2])

quaternion_integrator/quaternion_integrator.py
@@ -34,29 +34,31 @@ class QuaternionIntegrator(object):
     self.kT = 1.0
 
     
-  def FixmanTimeStep(self, dt):
+  def fixman_time_step(self, dt):
     ''' Take a timestep of length dt using the Fixman method '''
-    mobility  = self.mobility(self._position)
+    mobility  = self.mobility(self.position)
     mobility_half = np.linalg.cholesky(mobility)
     torque = self.torque_calculator(self.position)
-    noise = np.random.normal(0.0, 1.0, self.dim*4)
-    omega = mobility*torque + np.sqrt(2.0*self.kT/dt)*mobility_half*noise
+    noise = np.random.normal(0.0, 1.0, self.dim*3)
+    omega = (np.inner(mobility, torque) + 
+             np.sqrt(2.0*self.kT/dt)*np.inner(mobility_half, noise))
 
     # Update each quaternion
     position_midpoint = []
     for i in range(self.dim):
-      quaternion_dt = Quaternion.FromRotation((omega[i:i+4])*dt/2.)
-      positon_midpoint.append(quaternion_dt*self.position[i])
+      quaternion_dt = Quaternion.from_rotation((omega[i:i+3])*dt/2.)
+      position_midpoint.append(quaternion_dt*self.position[i])
       
     mobility_tilde = self.mobility(position_midpoint)
     torque_tilde = self.torque_calculator(position_midpoint)
     mobility_half_inv = np.linalg.inv(mobility_half)
-    omega_tilde = (mobility_tilde*torque_tilde +
-                   np.sqrt(2*self.kT/dt)*mobility_tilde*mobility_half_inv*noise)
+    omega_tilde = (np.inner(mobility_tilde, torque_tilde) +
+                   np.sqrt(2*self.kT/dt)*
+                   np.inner(mobility_tilde, np.inner(mobility_half_inv, noise)))
     
     new_position = []
     for i in range(self.dim):
-      quaternion_dt = Quaternion.FromRotation((omega_tilde[i:i+4])*dt/2.)
+      quaternion_dt = Quaternion.from_rotation((omega_tilde[i:i+3])*dt/2.)
       new_position.append(quaternion_dt*self.position[i])
       
     self.position = new_position

quaternion_integrator/quaternion_test.py
@@ -13,7 +13,7 @@ class TestQuaternion(unittest.TestCase):
     # Generate a random rotation vector
     phi = np.random.rand(3)
     phi_norm = np.linalg.norm(phi)
-    theta = Quaternion.FromRotation(phi)
+    theta = Quaternion.from_rotation(phi)
     
     self.assertAlmostEqual(theta.entries[0], np.cos(phi_norm/2.))
     self.assertAlmostEqual(theta.entries[1], np.sin(phi_norm/2.)*phi[0]/phi_norm)
@@ -57,7 +57,7 @@ class TestQuaternion(unittest.TestCase):
     p3 = np.sqrt(1. - s**2 - p1**2 - p2**2)
     theta = Quaternion(np.array([s, p1, p2, p3]))
     
-    R = theta.RotationMatrix()
+    R = theta.rotation_matrix()
 
     self.assertAlmostEqual(R[0][0], 2.*(theta.s**2 + theta.p[0]**2 - 0.5))
     self.assertAlmostEqual(R[0][1], 2.*(theta.p[0]*theta.p[1] + 

quaternion_integrator/tetrahedron/tetrahedron.py
@@ -1,7 +1,6 @@
 ''' 
-Script to test a tetrahedron near a wall.
-The wall is at z = -h, and the tetrahedron's "top" vertex is
-fixed at (0, 0, 0).  
+Script to test a tetrahedron near a wall.  The wall is at z = -h, and
+the tetrahedron's "top" vertex is fixed at (0, 0, 0).
 '''
 
 import numpy as np
@@ -12,7 +11,7 @@ ETA = 1.0   # Fluid viscosity.
 A = 0.1     # Particle Radius.
 H = 10.     # Distance to wall.
 
-def TetrahedronMobility(self, position):
+def tetrahedron_mobility(self, position):
   '''
   Calculate the mobility, torque -> angular velocity, at position 
   In this case, position is length 1, as there is just 1 quaternion.
@@ -23,16 +22,17 @@ def TetrahedronMobility(self, position):
   M (3N x 3N) is the singular image stokeslet for a point force near a wall, but
   we've replaced the diagonal piece by 1/(6 pi eta a).
   '''
-  r_vectors = GetRVectors(position[0])
-  M = ImageSingularStokeslet(position[0], r_vectors)
-  R = CalculateR(position[0], r_vectors)
+  r_vectors = get_r_vectors(position[0])
+  moblity = image_singular_stokeslet(position[0], r_vectors)
+  rotation_matrix = calculate_rot_matrix(position[0], r_vectors)
   
-  total_mobility = np.inner(R, np.inner(np.inverse(M), R.T))
-
+  total_mobility = np.inner(rotation_matrix,
+                            np.inner(np.inverse(mobility),
+                                     rotation_matrix.T))
   return total_mobility
 
 
-def ImageSingularStokeslet(quaternion, r_vectors):
+def image_singular_stokeslet(quaternion, r_vectors):
   ''' Calculate the image system for the singular stokeslet (M above).'''
   mobility = np.array([zeros(9) for _ in range(9)])
   # Loop through particle interactions
@@ -52,15 +52,15 @@ def ImageSingularStokeslet(quaternion, r_vectors):
               (l == m)*1./r_norm + r_particles[l]*r_particles[m]/(r_norm**3) -
               (l == m)*1./r_ref_norm + r_reflect[l]*r_reflect[m]/(r_ref_norm**3))
         # Add Doublet.
-        mobility[i*3:(i*3 + 3), j*3:(j*3 + 3)] += 2.*H*StokesDoublet(r_particles)
+        mobility[i*3:(i*3 + 3), j*3:(j*3 + 3)] += 2.*H*stokes_doublet(r_particles)
         # Add Potential Dipole.
-        mobility[i*3:(i*3 + 3), j*3:(j*3 + 3)] -= H*H*PotentialDipole(r_particles)
+        mobility[i*3:(i*3 + 3), j*3:(j*3 + 3)] -= H*H*potential_dipole(r_particles)
       else:
         mobility[i*3:(i*3 + 3), j*3:(j*3 + 3)] = 1./(6*np.pi*ETA*A)*np.identity(3)
       
   return mobility
               
-def StokesDoublet(r):
+def stokes_doublet(r):
   ''' Calculate stokes doublet from direction, strength, and r. '''
   r_norm = np.linalg.norm(r)
   e3 = np.array([0., 0., 1.])
@@ -71,7 +71,7 @@ def StokesDoublet(r):
   doublet = doublet/(8*np.pi*(r_norm**3))
   return doublet
 
-def PotentialDipole(r):
+def potential_dipole(r):
   ''' Calculate potential dipole. '''
   r_norm = np.linalg.norm(r)
   dipole = np.identity(3) - 3.*np.outer(r, r)/(r_norm**2)
@@ -81,7 +81,7 @@ def PotentialDipole(r):
   return dipole
 
   
-def CalculateR(quaternion, r_vectors):
+def calculate_rot_matrix(quaternion, r_vectors):
   ''' Calculate R, 9 by 3 matrix of cross products for r_i. '''
   
   # Create the 9 x 3 matrix.  Each 3x3 block is the matrix for a cross
@@ -102,7 +102,7 @@ def CalculateR(quaternion, r_vectors):
       ])
 
 
-def GetRVectors(quaternion):
+def get_r_vectors(quaternion):
   ''' Calculate r_i from a given quaternion. 
   The initial configuration is hard coded here but can be changed by
   considering an initial quaternion not equal to the identity rotation.
@@ -125,7 +125,7 @@ def GetRVectors(quaternion):
   initial_r2 = np.array([-1., -1./np.sqrt(3.), -2.*np.sqrt(2.)/np.sqrt(3.)])
   initial_r3 = np.array([1., -1./np.sqrt(3.), -2.*np.sqrt(2.)/np.sqrt(3.)])
   
-  rotation_matrix = quaternion.RotationMatrix()
+  rotation_matrix = quaternion.rotation_matrix()
 
   r1 = np.dot(rotation_matrix, initial_r1)
   r2 = np.dot(rotation_matrix, initial_r2)
@@ -134,13 +134,13 @@ def GetRVectors(quaternion):
   return [r1, r2, r3]
 
   
-def TorqueCalculator(position):
+def torque_calculator(position):
   ''' 
   Calculate torque based on position, given as a length
   1 list of quaternions (1 quaternion).  
   '''
-  r_vectors = GetRVectors(position[0])
-  R = CalculateR(position[0], r_vectors)
+  r_vectors = get_r_vectors(position[0])
+  R = calculate_rot_matrix(position[0], r_vectors)
   
   # Gravity
   g = np.array([0., 0., -1., 0., 0., -1., 0., 0., -1.])

quaternion_integrator/tetrahedron/tetrahedron_test.py
@@ -16,7 +16,7 @@ class TestTetrahedron(unittest.TestCase):
     # Identity quaternion.
     theta = Quaternion([1., 0., 0., 0.])
 
-    r_vectors = tetrahedron.GetRVectors(theta)
+    r_vectors = tetrahedron.get_r_vectors(theta)
     
     #Check r1
     self.assertAlmostEqual(r_vectors[0][0], 0.)
@@ -36,7 +36,7 @@ class TestTetrahedron(unittest.TestCase):
     # Try quaternion that flips tetrahedron 180 degrees, putting it upside down.
     theta = Quaternion([0., -1., 0., 0.])
 
-    r_vectors = tetrahedron.GetRVectors(theta)
+    r_vectors = tetrahedron.get_r_vectors(theta)
     
     #Check r1
     self.assertAlmostEqual(r_vectors[0][0], 0.)
@@ -57,7 +57,7 @@ class TestTetrahedron(unittest.TestCase):
     ''' Test stokes doublet when r = e1. '''
     r = np.array([1., 0., 0.])
     
-    doublet = tetrahedron.StokesDoublet(r)
+    doublet = tetrahedron.stokes_doublet(r)
     
     actual = (1./(8*np.pi))*np.array([
         [0., 0., 1.],
@@ -72,19 +72,19 @@ class TestTetrahedron(unittest.TestCase):
     ''' Test torque for a couple different configurations. '''
     # Identity quaternion.
     theta = Quaternion([1., 0., 0., 0.])
-    torque = tetrahedron.TorqueCalculator([theta])
+    torque = tetrahedron.torque_calculator([theta])
     for k in range(3):
       self.assertAlmostEqual(torque[k], 0.)
       
     # Upside down.
     theta = Quaternion([0., 1., 0., 0.])
-    torque = tetrahedron.TorqueCalculator([theta])
+    torque = tetrahedron.torque_calculator([theta])
     for k in range(3):
       self.assertAlmostEqual(torque[k], 0.)
 
     # Sideways.
     theta = Quaternion([1/np.sqrt(2.), 1./np.sqrt(2.), 0., 0.])
-    torque = tetrahedron.TorqueCalculator([theta])
+    torque = tetrahedron.torque_calculator([theta])
     self.assertAlmostEqual(torque[0], 2*np.sqrt(6.))
     for k in range(1, 3):
       self.assertAlmostEqual(torque[k], 0.)
@@ -95,6 +95,3 @@ class TestTetrahedron(unittest.TestCase):
       
 if __name__ == '__main__':
   unittest.main()
-    
-
-