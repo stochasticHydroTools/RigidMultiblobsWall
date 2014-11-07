Fixed rotation matrix and quaternion multiplication.

quaternion_integrator/quaternion.py
@@ -31,7 +31,7 @@ class Quaternion(object):
     s = (self.s*other.s - 
          np.dot(self.p, other.p))
     p = (self.s*other.p + other.s*self.p
-         - np.cross(self.p, other.p))
+         + np.cross(self.p, other.p))
     return Quaternion(np.concatenate(([s], p)))
 
 

quaternion_integrator/tetrahedron/tetrahedron_test.py
@@ -116,7 +116,7 @@ class TestTetrahedron(unittest.TestCase):
     # Sideways.
     theta = Quaternion([1/np.sqrt(2.), 1./np.sqrt(2.), 0., 0.])
     torque = tetrahedron.gravity_torque_calculator([theta])
-    self.assertAlmostEqual(torque[0], 2*np.sqrt(6.))
+    self.assertAlmostEqual(torque[0], -2*np.sqrt(6.))
     for k in range(1, 3):
       self.assertAlmostEqual(torque[k], 0.)
 