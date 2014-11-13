c3f14f394d41be0f92d5234530e5ccaf3c5ca897
Steven Delong <steven.d.delong@gmail.com>
2014-11-13 15:06:25 -0500

    Added free mobility test.

--- quaternion_integrator/tetrahedron/tetrahedron.py
+++ quaternion_integrator/tetrahedron/tetrahedron.py
@@ -13,6 +13,9 @@ import uniform_analyzer as ua
 import cProfile, pstats, StringIO
 # import tetrahedron_ext
 #  Parameters. TODO: perhaps there's a better way to do this.  Input file?
+
+# TODO: Move the fluid dynamics specific stuff (mobilities,etc) to a diff file.
+
 PROFILE = False  # Do we profile this run?
 
 ETA = 1.0   # Fluid viscosity.
@@ -32,11 +35,9 @@ def identity_mobility(position):
 def test_mobility(position):
   ''' Simple mobility that's not divergence free. '''
   r_vectors = get_r_vectors(position[0])
-  mobility = test_image_singular_stokeslet(r_vectors)
-  rotation_matrix = calculate_rot_matrix(r_vectors)
-  total_mobility = np.linalg.inv(np.dot(rotation_matrix.T,
-                                        np.dot(np.linalg.inv(mobility),
-                                               rotation_matrix)))
+  total_mobility = np.array([np.zeros(3) for _ in range(3)])
+  for k in range(3):
+    total_mobility[k, k] = r_vectors[k][2]**2 + 1.
   return total_mobility
 
 
@@ -61,6 +62,25 @@ def torque_mobility(r_vectors):
   '''  
   mobility = image_singular_stokeslet(r_vectors)
   rotation_matrix = calculate_rot_matrix(r_vectors)
+  total_mobility = np.linalg.inv(np.dot(rotation_matrix.T,
+                                        np.dot(np.linalg.inv(mobility),
+                                               rotation_matrix)))
+  return total_mobility
+
+
+def rpy_torque_mobility(r_vectors):
+  '''
+  Calculate the mobility, torque -> angular velocity, at position 
+  In this case, position is length 1, as there is just 1 quaternion.
+  The mobility is equal to R M^-1 R^t where R is 3N x 3 (9 x 3)
+  Rx = r cross x
+  r is the distance from the fixed vertex of the tetrahedron to
+  each other vertex (a length 3N vector).
+  M (3N x 3N) is the singular image stokeslet for a point force near a wall, but
+  we've replaced the diagonal piece by 1/(6 pi eta a).
+  '''  
+  mobility = rotne_prager_tensor(r_vectors, ETA, A)
+  rotation_matrix = calculate_rot_matrix_cm(r_vectors)
   total_mobility = np.linalg.inv(np.dot(rotation_matrix.T,
                                         np.dot(np.linalg.inv(mobility),
                                                rotation_matrix)))
@@ -122,7 +142,8 @@ def doublet_and_dipole(r, h):
   ''' 
   Just keep the pieces of the potential dipole and the doublet
   that we need for the image system.  No point in calculating terms that will cancel.
-  This function includes the prefactors of 2H and H**2.
+  This function includes the prefactors of 2H and H**2.  
+  Seems to be significantly faster.
   '''
   r_norm = np.linalg.norm(r)
   e3 = np.array([0., 0., 1.])
@@ -131,15 +152,35 @@ def doublet_and_dipole(r, h):
   return doublet_and_dipole
 
 
-def test_image_singular_stokeslet(r_vectors):
-  ''' A test non-divergence free stokeslet to use with test mobility. '''
-  mobility = np.array([np.zeros(9) for _ in range(9)])
-  for j in range(3):
-    for k in range(3):
-      mobility[j*3 + k, j*3 + k] = 1.0 + 100.*r_vectors[j][k]**2
+def rotne_prager_tensor(r_vectors, eta, a):
+  ''' Calculate free rotne prager tensor for particles at locations given by
+  r_vectors (list of 3 dimensional locationis) of radius a.'''
+  num_particles = len(r_vectors)
+  mobility = np.array([np.zeros(3*num_particles) for _ in range(3*num_particles)])
+  for j in range(num_particles):
+    for k in range(num_particles):
+      if j != k:
+        # Particle interaction, rotne prager.
+        r = r_vectors[j] - r_vectors[k]
+        r_norm = np.linalg.norm(r)
+        if r_norm > 2.*a:
+          # Constants for far RPY tensor, taken from OverdampedIB paper.
+          C1 = 3.*a/(4.*r_norm) + (a**3)/(2.*r_norm**3)
+          C2 = 3.*a/(4.*r_norm) - (3.*a**3)/(2.*r_norm**3)
+        elif r_norm <= 2.*a:
+          # This is for the close interaction, 
+          #  Call C3 -> C1 and C4 -> C2
+          C1 = 1 - 9.*r_norm/(32.*a)
+          C2 = 3*r_norm/(32.*a)
+        mobility[(j*3):(j*3 + 3), (k*3):(k*3 + 3)] = (1./(6.*np.pi*eta*a)*(
+            C1*np.identity(3) + C2*np.outer(r, r)/(r_norm**2)))
+      elif j == k:
+        # j == k, diagonal block.
+        mobility[(j*3):(j*3 + 3), (k*3):(k*3 + 3)] = ((1./(6.*np.pi*eta*a))*
+                                                      np.identity(3))
   return mobility
-
   
+
 def calculate_rot_matrix(r_vectors):
   ''' Calculate R, 3N by 3 matrix of cross products for r_i. '''
   
@@ -152,6 +193,26 @@ def calculate_rot_matrix(r_vectors):
   for k in range(len(r_vectors)):
     r_vectors[k] = r_vectors[k] - np.array([0., 0., H])
 
+    # Current r cross x matrix block.
+    block = np.array(
+        [[0.0, r_vectors[k][2], -1.*r_vectors[k][1]],
+        [-1.*r_vectors[k][2], 0.0, r_vectors[k][0]],
+        [r_vectors[k][1], -1.*r_vectors[k][0], 0.0]])
+
+    if rot_matrix is None:
+      rot_matrix = block
+    else:
+      rot_matrix = np.concatenate([rot_matrix, block], axis=0)
+
+  return rot_matrix
+
+
+def calculate_rot_matrix_cm(r_vectors):
+  ''' Calculate R, 3N by 3 matrix of cross products for r_i relative to 0, 0, 0 '''
+  # Create the 3N x 3 matrix.  Each 3x3 block is the matrix for a cross
+  # product with one of the r_vectors.  Cross is relative to (0, 0, 0)   
+  rot_matrix = None
+  for k in range(len(r_vectors)):
     # Current r cross x matrix block.
     block = np.array(
         [[0.0, r_vectors[k][2], -1.*r_vectors[k][1]],
@@ -287,15 +348,15 @@ if __name__ == "__main__":
 
   # Script to run the various integrators on the quaternion.
   initial_position = [Quaternion([1., 0., 0., 0.])]
-  fixman_integrator = QuaternionIntegrator(tetrahedron_mobility,
+  fixman_integrator = QuaternionIntegrator(test_mobility,
                                            initial_position, 
                                            gravity_torque_calculator)
 
-  rfd_integrator = QuaternionIntegrator(identity_mobility, 
+  rfd_integrator = QuaternionIntegrator(test_mobility, 
                                         initial_position, 
                                         gravity_torque_calculator)
 
-  em_integrator = QuaternionIntegrator(tetrahedron_mobility, 
+  em_integrator = QuaternionIntegrator(test_mobility, 
                                        initial_position, 
                                        gravity_torque_calculator)
   # Get command line parameters
