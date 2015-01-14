bca48a8c10dbc5c3a2e055cdaae66f94b663e80b
Steven Delong <steven.d.delong@gmail.com>
2015-01-14 18:09:04 -0500

    Rotational MSD will now save all data and we can plot any component.

--- quaternion_integrator/tetrahedron/plot_rotational_msd.py
+++ quaternion_integrator/tetrahedron/plot_rotational_msd.py
@@ -11,19 +11,30 @@ import numpy as np
 import cPickle
 from quaternion_integrator.tetrahedron.tetrahedron_rotational_msd import MSDStatistics
 
-def plot_time_dependent_msd(msd_statistics):
-  ''' Plot the rotational MSD as a function of time.'''
+def plot_time_dependent_msd(msd_statistics, ind):
+  ''' 
+  Plot the <ind> entry of the rotational MSD as 
+  a function of time.  This uses the msd_statistics object
+  that is saved by tetrahedron_rotational_msd.py.
+  '''
   # Types of lines for different dts.
   dt_styles = ['', ':', '--']
   scheme_colors = ['b','g','r']
   scheme_num = 0
+
   for scheme in msd_statistics.data.keys():
     dt_num = 0
     pyplot.figure(scheme_num)
     for dt in msd_statistics.data[scheme].keys():
-      pyplot.errorbar(msd_statistics.data[scheme][dt][0], 
-                      msd_statistics.data[scheme][dt][1],
-                      yerr = 2.*np.array(msd_statistics.data[scheme][dt][2]),
+      # Extract the entry specified by ind to plot.
+      num_steps = len(msd_statistics.data[scheme][dt][0])
+      msd_entries = [msd_statistics.data[scheme][dt][1][_][ind[0]][ind[1]] 
+                     for _ in range(num_steps)]
+      msd_entries_std = [msd_statistics.data[scheme][dt][2][_][ind[0]][ind[1]] 
+                         for _ in range(num_steps)]
+      pyplot.errorbar(msd_statistics.data[scheme][dt][0],
+                      msd_entries,
+                      yerr = 2.*np.array(msd_entries_std),
                       fmt = scheme_colors[scheme_num] + dt_styles[dt_num],
                       label = '%s, dt=%s' % (scheme, dt))
       dt_num += 1
@@ -41,4 +52,4 @@ if __name__ == "__main__":
   with open(data_name, 'rb') as f:
     msd_statistics = cPickle.load(f)
 
-  plot_time_dependent_msd(msd_statistics)
+  plot_time_dependent_msd(msd_statistics, [1, 1])
