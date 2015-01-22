c5b1ef0437b9951ae41ae1ee6e73f8958c82c925
Steven Delong <steven.d.delong@gmail.com>
2015-01-22 17:16:49 -0500

    fixed fluid mobility tests.

--- fluids/mobility_test.py
+++ fluids/mobility_test.py
@@ -6,7 +6,7 @@ import numpy as np
 import random
 from quaternion_integrator.quaternion import Quaternion
 from quaternion_integrator.quaternion_integrator import QuaternionIntegrator
-import mobility
+import mobility as mb
 
 
 class TestMobility(unittest.TestCase):
@@ -18,7 +18,7 @@ class TestMobility(unittest.TestCase):
     ''' Test stokes doublet when r = e1. '''
     r = np.array([1., 0., 0.])
     
-    doublet = mobility.stokes_doublet(r)
+    doublet = mb.stokes_doublet(r)
     
     actual = (1./(8*np.pi))*np.array([
         [0., 0., 1.],
@@ -33,7 +33,7 @@ class TestMobility(unittest.TestCase):
   def test_stokes_dipole_e1(self):
     ''' Test dipole when r = e1. '''
     r = np.array([1., 0., 0.])
-    dipole = mobility.potential_dipole(r)
+    dipole = mb.potential_dipole(r)
     actual = (1./(4*np.pi))*np.array([
         [2., 0., 0.],
         [0., -1., 0.],
@@ -51,7 +51,8 @@ class TestMobility(unittest.TestCase):
     r_vectors = [np.array([0., 0., 0.]),
                  np.array([2., 2., 2.]),
                  np.array([1., 1., 1.])]
-    fluid_mobility = mobility.image_singular_stokeslet(r_vectors)
+    a = 0.5
+    fluid_mobility = mb.image_singular_stokeslet(r_vectors, a)
     # Test particle 1 to particle 0.
     block = fluid_mobility[0:3, 3:6]
     for j in range(3):
@@ -68,40 +69,31 @@ class TestMobility(unittest.TestCase):
     ''' Test that the finite size mobility goes to the oseen for small a.'''
     n_particles = 4
     a = 0.000001
-    old_a = mobility.A
-    mobility.A = a
     # Random configuration, all above wall.
     r_vectors = [np.random.normal(5., 1., 3) for _ in range(n_particles)]
 
-    mobility_finite = mobility.single_wall_fluid_mobility(r_vectors, 1., a)
-    mobility_point = mobility.image_singular_stokeslet(r_vectors)
+    mobility_finite = mb.single_wall_fluid_mobility(r_vectors, 1., a)
+    mobility_point = mb.image_singular_stokeslet(r_vectors, a)
     for j in range(3*n_particles):
       for k in range(3*n_particles):
         # We multiply by a to get reasonable numbers for mobility.
         self.assertAlmostEqual(a*mobility_finite[j,k], a*mobility_point[j,k])
-    mobility.A = old_a
 
 
   def test_image_system_spd(self):
     ''' Test that the image system for a singular stokeslet is SPD.'''
-    # First construct any random unit quaternion. Not uniform.
-    s = 2*random.random() - 1.
-    p1 = (2. - 2*np.abs(s))*random.random() - (1. - np.abs(s))
-    p2 = ((2. - 2.*np.abs(s) - 2.*np.abs(p1))*random.random() - 
-          (1. - np.abs(s) - np.abs(p1)))
-    p3 = np.sqrt(1. - s**2 - p1**2 - p2**2)
-    theta = Quaternion(np.array([s, p1, p2, p3]))
-
-    r_vectors = tetrahedron.get_r_vectors(theta)
-    stokeslet = mb.image_singular_stokeslet(r_vectors)
+    n_particles = 5
+    a = 1.0
+    r_vectors = [np.random.normal(10., 1., 3) for _ in range(n_particles)]
+    stokeslet = mb.image_singular_stokeslet(r_vectors, a)
     def is_pos_def(x):
       return np.all(np.linalg.eigvals(x) > 0)    
     
-    self.assertEqual(len(stokeslet), 9)
-    self.assertEqual(len(stokeslet[0]), 9)
+    self.assertEqual(len(stokeslet), 3*n_particles)
+    self.assertEqual(len(stokeslet[0]), 3*n_particles)
     self.assertTrue(is_pos_def(stokeslet))
-    for j in range(9):
-      for k in range(j+1, 9):
+    for j in range(3*n_particles):
+      for k in range(j+1, 3*n_particles):
         self.assertAlmostEqual(stokeslet[j, k], stokeslet[k, j])
 
   def test_rpy_spd(self):
@@ -113,7 +105,7 @@ class TestMobility(unittest.TestCase):
     # Random configuration.
     r_vectors = [np.random.normal(0., 1., 3) for _ in range(n_particles)]
     
-    rpy = tetrahedron.rotne_prager_tensor(r_vectors, 1., 1.)
+    rpy = mb.rotne_prager_tensor(r_vectors, 1., 1.)
     self.assertTrue(is_pos_def(rpy))
     for j in range(3*n_particles):
       for k in range(j+1, 3*n_particles):
