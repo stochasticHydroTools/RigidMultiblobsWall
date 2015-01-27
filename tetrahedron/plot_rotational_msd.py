033c6c3578c2fd1b2631f063b05faada5d9b979a
skwaap <steven.d.delong@gmail.com>
2015-01-27 17:21:59 -0500

    commit before merge.

--- tetrahedron/plot_rotational_msd.py
+++ tetrahedron/plot_rotational_msd.py
@@ -21,31 +21,43 @@ def plot_time_dependent_msd(msd_statistics, ind):
   ind = [row index, column index].
   '''
   # Types of lines for different dts.
+  write_data = True
+  if write_data:
+    np.set_printoptions(threshold=np.nan)
   dt_styles = ['', ':', '--', '-.']
   scheme_colors = ['b','g','r']
   scheme_num = 0
   num_err_bars = 12
-  for scheme in msd_statistics.data.keys():
-    dt_num = 0
-    for dt in msd_statistics.data[scheme].keys():
-      # Extract the entry specified by ind to plot.
-      num_steps = len(msd_statistics.data[scheme][dt][0])
-      # Don't put error bars at every point
-      err_idx = [int(num_steps*k/num_err_bars) for k in range(num_err_bars)]
-      msd_entries = np.array([msd_statistics.data[scheme][dt][1][_][ind[0]][ind[1]]
-                     for _ in range(num_steps)])
-      msd_entries_std = np.array([msd_statistics.data[scheme][dt][2][_][ind[0]][ind[1]]
-                                  for _ in range(num_steps)])
-      pyplot.plot(msd_statistics.data[scheme][dt][0],
-                  msd_entries,
-                  scheme_colors[scheme_num] + dt_styles[dt_num],
-                  label = '%s, dt=%s' % (scheme, dt))
-      pyplot.errorbar(np.array(msd_statistics.data[scheme][dt][0])[err_idx],
-                      msd_entries[err_idx],
-                      yerr = 2.*msd_entries_std[err_idx],
-                      fmt = scheme_colors[scheme_num] + '.')
-      dt_num += 1
-    scheme_num += 1
+  with open("./MSD-component-%s-%s.txt" % (ind[0], ind[1]),'w') as f:
+    for scheme in msd_statistics.data.keys():
+      dt_num = 0
+      for dt in msd_statistics.data[scheme].keys():
+        # Extract the entry specified by ind to plot.
+        num_steps = len(msd_statistics.data[scheme][dt][0])
+        # Don't put error bars at every point
+        err_idx = [int(num_steps*k/num_err_bars) for k in range(num_err_bars)]
+        msd_entries = np.array([msd_statistics.data[scheme][dt][1][_][ind[0]][ind[1]]
+                                for _ in range(num_steps)])
+        msd_entries_std = np.array(
+          [msd_statistics.data[scheme][dt][2][_][ind[0]][ind[1]]
+           for _ in range(num_steps)])
+        pyplot.plot(msd_statistics.data[scheme][dt][0],
+                    msd_entries,
+                    scheme_colors[scheme_num] + dt_styles[dt_num],
+                    label = '%s, dt=%s' % (scheme, dt))
+        if write_data:
+          f.write("scheme %s \n" % scheme)
+          f.write("dt %s \n" % dt)
+          f.write("time: %s \n" % msd_statistics.data[scheme][dt][0])
+          f.write("MSD component: %s \n" % msd_entries)
+          f.write("Std Dev:  %s \n" % msd_entries_std)
+        
+        pyplot.errorbar(np.array(msd_statistics.data[scheme][dt][0])[err_idx],
+                        msd_entries[err_idx],
+                        yerr = 2.*msd_entries_std[err_idx],
+                        fmt = scheme_colors[scheme_num] + '.')
+        dt_num += 1
+      scheme_num += 1
   pyplot.title('MSD(t) for Tetrahedron')
   pyplot.ylabel('MSD')
   pyplot.xlabel('time')
@@ -63,4 +75,4 @@ if __name__ == "__main__":
   with open(data_name, 'rb') as f:
     msd_statistics = cPickle.load(f)
 
-  plot_time_dependent_msd(msd_statistics, [4, 4])
+  plot_time_dependent_msd(msd_statistics, [5, 5])
