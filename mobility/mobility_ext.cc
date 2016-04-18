54ead5e877495ed3a6abcd453d491438cd6c8c65
floren <fbusabiaga@gmail.com>
2016-04-18 16:27:46 -0400

    I simplify the function MobilityVectorProductOneParticle

--- mobility/mobility_ext.cc
+++ mobility/mobility_ext.cc
@@ -559,7 +559,11 @@ void MobilityVectorProductOneParticle(bp::list r_vectors,
   
   bp::numeric::array r_vector_1 = bp::extract<bp::numeric::array>(r_vectors[j]);
 
-  for (int k = 0; k < j; ++k) {
+  for (int k = 0; k < num_particles; ++k){
+    if(k == j){
+      continue;
+    }
+
     // Here notation is based on appendix C of the Swan and Brady paper:
     //  'Simulation of hydrodynamically interacting particles near a no-slip
     //   boundary.'
@@ -633,87 +637,12 @@ void MobilityVectorProductOneParticle(bp::list r_vectors,
     }
   }
   
-  if (j<num_particles-1){
-    for (int k = j+1; k < num_particles; ++k) {
-      // Here notation is based on appendix C of the Swan and Brady paper:
-      //  'Simulation of hydrodynamically interacting particles near a no-slip
-      //   boundary.'
-      bp::numeric::array  r_vector_2 = bp::extract<bp::numeric::array>(r_vectors[k]);
-      h = bp::extract<double>(r_vector_2[2]);
-      for (int l = 0; l < 3; ++l) {
-	R[l] = (bp::extract<double>(r_vector_1[l]) -
-		bp::extract<double>(r_vector_2[l]))/a;
-      }
-      for (int l = 0; l < 2; ++l) {
-	Rim[l] = (bp::extract<double>(r_vector_1[l]) -
-		bp::extract<double>(r_vector_2[l]))/a;
-      }
-      Rim[2] = (bp::extract<double>(r_vector_1[2]) -
-	      bp::extract<double>(r_vector_2[2]) + 2.0*h)/a;
-	      
-      double R_norm = 0.0;
-      double Rim_norm = 0.0;
-      for (int l = 0; l < 3; ++l) {
-	R_norm += R[l]*R[l];
-	Rim_norm += Rim[l]*Rim[l];
-      }
-      R_norm = sqrt(R_norm);
-
-      Rim_norm = sqrt(Rim_norm);
-      double* e = new double[3];
-      for (int l = 0; l < 3; ++l) {
-	e[l] = Rim[l]/Rim_norm;
-      }
-      double* e_3 = new double[3];
-      e_3[0] = 0.0;
-      e_3[1] = 0.0;
-      e_3[2] = e[2];
-      
-      double h_hat = h/(a*Rim[2]);
-      
-      if (R_norm > 2.0) {
-		  C1 = 3./(4.*R_norm) + 1./(2.*pow(R_norm,3));
-		  C2 = 3./(4.*R_norm) - 3./(2.*pow(R_norm,3));
-	  }
-	  else if (R_norm <= 2.0) {
-		  C1 = 1. - 9./32.*R_norm;
-		  C2 = 3./32.*R_norm;
-	  }
-
-      for (int l = 0; l < 3; ++l) {
-	for (int m = 0; m < 3; ++m) {	
-      // Usual RPY Tensor
-	  Mlm = (l == m ? 1.0 : 0.0)*C1 + R[l]*R[m]/pow(R_norm,2)*C2; 
-      // Taken from Appendix C expression for M_UF.
-	  Mlm +=
-	      (-0.25*(3.0*(1.0 - 6.0*h_hat*(1. - h_hat)*pow(e[2],2))/Rim_norm
-		      - 6.0*(1.0 - 5.0*pow(e[2],2))/(pow(Rim_norm,3))
-		      + 10.0*(1.0 - 7.0*pow(e[2],2))/(pow(Rim_norm,5)))*(e[l]*e[m])
-		- (0.25*(3.0*(1.0 + 2.0*h_hat*(1. - h_hat)*pow(e[2],2))/Rim_norm
-			+ 2.0*(1.0 - 3.0*pow(e[2],2))/(pow(Rim_norm,3))
-			- 2.0*(1.0 - 5.0*pow(e[2],2))/(pow(Rim_norm,5))))*(l == m ? 1.0 : 0.0)
-		+ 0.5*(3.0*h_hat*(1. - 6.0*(1. - h_hat)*pow(e[2],2))/Rim_norm
-		      - 6.0*(1.0 - 5.0*pow(e[2],2))/(pow(Rim_norm,3))
-		      + 10.0*(2.0 - 7.0*pow(e[2],2))/(pow(Rim_norm,5)))*(e[l]*e_3[m])
-		+ 0.5*(3.0*h_hat/Rim_norm - 10./(pow(Rim_norm,5)))*(e_3[l]*e[m])
-		- (3.0*(pow(h_hat,2))*(pow(e[2],2))/Rim_norm 
-		  + 3.0*(pow(e[2],2))/(pow(Rim_norm,3))
-		  + (2.0 - 15.0*pow(e[2],2))/(pow(Rim_norm,5)))*
-		(e_3[l]*e_3[m])/(pow(e[2],2)));
-		
-	  Mlm = Mlm*(1.0/(6.0*pi*eta*a));
-	  
-	  vector_res[l] += Mlm*bp::extract<double>(vector[k*3+m]);
-	}
-      }
-    }
-  }
   
-// Diagonal blocks, self mobility.
+  // Diagonal blocks, self mobility.
   h = bp::extract<double>(r_vector_1[2])/a;
   for (int l = 0; l < 3; ++l) {
     for (int m = 0; m < 3; ++m) {
-	  // RPY
+         // RPY
 	  Mlm = (l == m ? 1.0 : 0.0);
 	  // Wall
 	  Mlm += (
@@ -724,12 +653,7 @@ void MobilityVectorProductOneParticle(bp::list r_vectors,
 	  Mlm = Mlm*(1./(6.0*pi*eta*a));
 	  vector_res[l]+= Mlm*bp::extract<double>(vector[j*3+m]);
     }
-  }
-  
-  
-//   for(int i=0; i<3;i++){
-// 	  std::cout << "vectorres " << i << "  " << bp::extract<double>(vector_res[i]) << std::endl;
-//   }
+  } 
 }
 
 
