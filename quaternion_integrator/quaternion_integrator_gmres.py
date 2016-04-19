f0a34982d185fabb5338d9ff421ed299dce4af53
floren <fbusabiaga@gmail.com>
2016-04-19 13:10:04 -0400

    I deleted some functions from quaternion_integrator_gmres.py

--- quaternion_integrator/quaternion_integrator_gmres.py
+++ quaternion_integrator/quaternion_integrator_gmres.py
@@ -105,87 +105,6 @@ class QuaternionIntegratorGMRES(object):
     self.avg_velocity = 0.0
     self.avg_omega = 0.0
 
-  def fixman_time_step(self, dt):
-    ''' Take a timestep of length dt using the Fixman method '''
-    # Try to take steps until one is valid
-    while True:
-      if self.has_location:
-        # Handle integrator with location as well.
-        mobility  = self.mobility(self.location, self.orientation)
-        mobility_half = np.linalg.cholesky(mobility)
-        noise = np.random.normal(0.0, 1.0, self.dim*6)
-        force = self.force_calculator(self.location, self.orientation)
-        torque = self.torque_calculator(self.location, self.orientation)
-        velocity_and_omega = (np.dot(mobility, np.concatenate([force, torque])) +
-                              np.sqrt(4.0*self.kT/dt)*
-                              np.dot(mobility_half, noise))
-        velocity = velocity_and_omega[0:(3*self.dim)]
-        omega = velocity_and_omega[(3*self.dim):(6*self.dim)]
-      else:
-        mobility  = self.mobility(self.orientation)
-        mobility_half = np.linalg.cholesky(mobility)
-        noise = np.random.normal(0.0, 1.0, self.dim*3)
-        torque = self.torque_calculator(self.orientation)
-        omega = (np.dot(mobility, torque) + 
-               np.sqrt(4.0*self.kT/dt)*np.dot(mobility_half, noise))
-        
-      # Update each quaternion at a time.
-      orientation_midpoint = []
-      for i in range(self.dim):
-        quaternion_dt = Quaternion.from_rotation((omega[(i*3):(i*3+3)])*dt/2.)
-        orientation_midpoint.append(quaternion_dt*self.orientation[i])
-
-      if self.has_location:
-        location_midpoint = self.location + 0.5*dt*velocity
-        if not self.check_new_state(location_midpoint, orientation_midpoint):
-          # restart the step.
-          continue
-
-      if self.has_location:
-        mobility_tilde = self.mobility(location_midpoint, orientation_midpoint)
-        noise = noise + np.random.normal(0.0, 1.0, self.dim*6)
-        force_tilde = self.force_calculator(location_midpoint, orientation_midpoint)
-        torque_tilde = self.torque_calculator(location_midpoint, orientation_midpoint)
-        mobility_half_inv = np.linalg.inv(mobility_half)
-        velocity_and_omega_tilde = (
-          np.dot(mobility_tilde, 
-                 np.concatenate([force_tilde, torque_tilde])) + np.sqrt(self.kT/dt)*
-          np.dot(mobility_tilde, np.dot(mobility_half_inv.T, noise)))
-        velocity_tilde = velocity_and_omega_tilde[0:(3*self.dim)]
-        self.avg_velocity += np.linalg.norm(velocity_tilde)
-        omega_tilde = velocity_and_omega_tilde[(3*self.dim):(6*self.dim)]
-        self.avg_omega += np.linalg.norm(omega_tilde)
-      
-      else:
-        mobility_tilde = self.mobility(orientation_midpoint)
-        noise = noise + np.random.normal(0.0, 1.0, self.dim*3)
-        torque_tilde = self.torque_calculator(orientation_midpoint)
-        mobility_half_inv = np.linalg.inv(mobility_half)
-        omega_tilde = (
-          np.dot(mobility_tilde, torque_tilde) + np.sqrt(self.kT/dt)*
-          np.dot(mobility_tilde, np.dot(mobility_half_inv.T, noise)))
-        
-      new_orientation = []
-      for i in range(self.dim):
-        quaternion_dt = Quaternion.from_rotation((omega_tilde[(i*3):(i*3+3)])*dt)
-        new_orientation.append(quaternion_dt*self.orientation[i])
-
-      # Check that the new state is admissible. Re-take the step 
-      # (with tail recursion) if the new state is invalid.
-      if self.has_location:
-        new_location = self.location + dt*velocity_tilde
-        if self.check_new_state(new_location, new_orientation):
-          self.location = new_location
-          self.orientation = new_orientation
-          self.successes += 1
-          return
-      else:
-        if self.check_new_state(None, new_orientation):
-          self.orientation = new_orientation
-          self.successes += 1
-          return
-
-
   def rfd_time_step(self, dt):
     ''' Take a timestep of length dt using the RFD method '''
     # Attempt steps until we find a valid endpoint.  
@@ -204,31 +123,22 @@ class QuaternionIntegratorGMRES(object):
 	  print "=================================================== "
 	  print "==== Compute matrices and vectors for GMRES iteration  ====== "
 	  print "=================================================== "
-	  (J_tot,self_mobility_body,chol_mobility_blobs_each_body,\
-			    r_vectors_ite, rotation_matrix_ite) = \
-		self.matrices_for_GMRES_ite(self.location, self.orientation, self.initial_config)
-	      
-	  print "6*6 mobility = "
-	  for i in range(6):
-	    for j in range(i+1):
-	      if abs(self_mobility_body[i][j])>1e-10:
-	       print i,j
-	       print self_mobility_body[i][j]
-	  raw_input()
+	  (J_tot,self_mobility_body, chol_mobility_blobs_each_body,\
+             r_vectors_ite, rotation_matrix_ite) = \
+             self.matrices_for_GMRES_ite(self.location, self.orientation, self.initial_config)
 	elif self.solver == 2:
 	  print "=================================================== "
 	  print "==== Compute configs for Direct Solver iteration  ====== "
 	  print "=================================================== "
 	  ### IMPLEMENT ROUTINE FOR DIRECT SOLVER WHICH RETURN ROTATION MATRIX AND r_vectros
 	  (r_vectors_ite, rotation_matrix_ite) = \
-		self.matrices_for_direct_ite(self.location, self.orientation, self.initial_config)
+              self.matrices_for_direct_ite(self.location, self.orientation, self.initial_config)
 
         print "=================================================== "
         print "==== Compute forces and torques on rods  ====== "
         print "=================================================== "
-        #force = self.force_calculator(r_vectors_ite)
-        #torque = self.torque_calculator(r_vectors_ite, rotation_matrix_ite)
-      
+        # force = self.force_calculator(r_vectors_ite)
+        # torque = self.torque_calculator(r_vectors_ite, rotation_matrix_ite)
         random_forces_blobs = 1.0*(2.0*np.random.random(self.Nblobs*3)-1.0)*6.0*m.pi*self.A
         random_forcing_rods = self.force_slip(r_vectors_ite, rotation_matrix_ite,random_forces_blobs)
         
@@ -248,7 +158,7 @@ class QuaternionIntegratorGMRES(object):
 	  print "=================================================== "
 	  print "==== Direct Solver  ====== "
 	  print "=================================================== " 
-	  (mobility, res_blobs)  = self.mobility(r_vectors_ite, rotation_matrix_ite)
+          (mobility, res_blobs)  = self.mobility(r_vectors_ite, rotation_matrix_ite)
 	  #resistance = self.resistance_blobs(r_vectors_ite)
 	  #lambda_tilde = np.dot(res_blobs, slip)
 	  #force_slip = -self.force_slip(r_vectors_ite, rotation_matrix_ite ,lambda_tilde)
@@ -257,13 +167,6 @@ class QuaternionIntegratorGMRES(object):
 	  
 	  force_deterministic = np.concatenate([force, torque]) #+ force_slip
 	  
-	  #print "12*12 mobility = "
-	  #for i in range(12):
-	    #for j in range(12):
-	      #if abs(mobility[i][j])>1e-10:
-	       #print i,j
-	       #print mobility[i][j]
-	  #raw_input()
 	  
 	  
 	  # Add noise
@@ -352,7 +255,6 @@ class QuaternionIntegratorGMRES(object):
         print velocity
 	print "omega = "
         print omega
-        raw_input()
         
         self.avg_velocity += np.linalg.norm(velocity)
         self.avg_omega += np.linalg.norm(omega)
@@ -367,8 +269,8 @@ class QuaternionIntegratorGMRES(object):
           self.veltot.append(velocity[3*i:3*(i+1)])
           self.omegatot.append(omega[3*i:3*(i+1)])
           
-           ## TO COMMENT, ONLY FOR HYDRO TESTS
-          self.mob_coeff.append(mobility)
+          ## TO COMMENT, ONLY FOR HYDRO TESTS
+          # self.mob_coeff.append(mobility)
 
           ## TO COMMENT, ONLY FOR HYDRO TESTS
           new_location.append(self.location[i] + 0.01*np.array([0.0, 0.0, (-1.0)**(float(i+1))]))
@@ -418,123 +320,6 @@ class QuaternionIntegratorGMRES(object):
           return
 
     
-  def additive_em_time_step(self, dt):
-    ''' 
-    Take a simple Euler Maruyama step assuming that the mobility is
-    constant.  This for testing and debugging.  We also use it to make sure
-    that we need the drift for the correct distribution, etc.
-    '''
-    if self.has_location:
-      mobility  = self.mobility(self.location, self.orientation)
-      mobility_half = np.linalg.cholesky(mobility)
-      noise = np.random.normal(0.0, 1.0, self.dim*6)
-      force = self.force_calculator(self.location, self.orientation)
-      torque = self.torque_calculator(self.location, self.orientation)
-      velocity_and_omega = (np.dot(mobility, np.concatenate([force, torque])) +
-                            np.sqrt(2.0*self.kT/dt)*
-                            np.dot(mobility_half, noise))
-      velocity = velocity_and_omega[0:(3*self.dim)]
-      omega = velocity_and_omega[(3*self.dim):(6*self.dim)]
-      new_location = self.location + dt*velocity
-      new_orientation = []
-      for i in range(self.dim):
-        quaternion_dt = Quaternion.from_rotation((omega[(i*3):(i*3+3)])*dt)
-        new_orientation.append(quaternion_dt*self.orientation[i])
-      if self.check_new_state(new_location, new_orientation):
-        self.successes += 1
-        self.orientation = new_orientation
-        self.location = new_location
-    else:
-      mobility = self.mobility(self.orientation)
-      mobility_half = np.linalg.cholesky(mobility)
-      torque = self.torque_calculator(self.orientation)
-      noise = np.random.normal(0.0, 1.0, self.dim*3)
-      omega = (np.dot(mobility, torque) + 
-               np.sqrt(2.0*self.kT/dt)*np.dot(mobility_half, noise))
-      new_orientation = []
-      for i in range(self.dim):
-        quaternion_dt = Quaternion.from_rotation((omega[(i*3):(i*3+3)])*dt)
-        new_orientation.append(quaternion_dt*self.orientation[i])
-
-      if self.check_new_state(None, new_orientation):
-        self.successes += 1
-        self.orientation = new_orientation
-
-  def estimate_divergence(self):
-    ''' 
-    Estimate the divergence term with a deterministic
-    finite difference approach. This is for a single quaternion.
-    '''
-    delta = 1e-6
-    div_term = np.zeros(3)
-    for k in range(3):
-      omega = np.zeros(3)
-      omega[k] = 1.
-      quaternion_dt = Quaternion.from_rotation(omega*delta/2.)
-      quaternion_tilde_1 = quaternion_dt*self.orientation[0]
-      quaternion_dt = Quaternion.from_rotation(-1.*omega*delta/2.)
-      quaternion_tilde_2 = quaternion_dt*self.orientation[0]
-      div_term += np.inner((self.mobility([quaternion_tilde_1]) -
-                            self.mobility([quaternion_tilde_2])),
-                           omega/delta)
-      
-    return div_term
-
-    
-  def estimate_drift_and_covariance(self, dt, n_steps, scheme):
-    ''' Emperically estimate the drift and covariance term in the absence of torque. 
-    For now this is just without location.  TODO: add location.'''
-
-    if self.dim > 1:
-      # For now, hard code to 1 dimensional integrator.
-      raise NotImplementedError('Drift and Covariance estimation only implemented for '
-                                '1-d integrators.')
-
-    old_torque = self.torque_calculator
-    if self.has_location:
-      def zero_torque(orientation, location):
-        return np.zeros(3*len(orientation))
-      def zero_force(orientation, location):
-        return np.zeros(3*len(orientation))
-      old_force = self.force_calculator
-      self.force_calculator = zero_force
-      initial_location = self.location
-    else:
-      def zero_torque(orientation):
-        return np.zeros(3*len(orientation))
-    self.torque_calculator = zero_torque
-    initial_orientation = self.orientation
-
-    drift_samples = []
-    covariance_samples = []
-    for k in range(n_steps):
-      if scheme == 'FIXMAN':
-        self.fixman_time_step(dt)
-      elif scheme == 'RFD':
-        self.rfd_time_step(dt)
-      else:
-        raise Exception('scheme must be FIXMAN or RFD for drift estimation.')
-
-      for l in range(self.dim):
-        orientation_increment = self.orientation[l]*initial_orientation[l].inverse()
-        drift = orientation_increment.rotation_angle()
-        if self.has_location:
-          drift = np.concatenate([self.location[l] - initial_location[l], drift])
-      drift_samples.append(drift)
-      covariance_samples.append(np.outer(drift, drift))
-      self.orientation = initial_orientation
-      if self.has_location:
-        self.location = initial_location
-
-    avg_drift = np.mean(drift_samples, axis=0)/dt
-    avg_covariance = np.mean(covariance_samples, axis=0)/(2.*dt)
-
-    # Reset torque calculator
-    self.torque_calculator = old_torque
-    if self.has_location:
-      self.force_calculator = old_force
-
-    return [avg_drift, avg_covariance]
       
       
   def check_new_state(self, location, orientation):
