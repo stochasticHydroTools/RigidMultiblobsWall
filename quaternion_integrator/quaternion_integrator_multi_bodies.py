a6b43ea5a9f34074ba322bcf542107abeb84794d
fbusabiaga <fbusabiaga@gmail.com>
2016-05-03 22:45:37 -0400

    Second order Adams-Bashforth implemented

--- quaternion_integrator/quaternion_integrator_multi_bodies.py
+++ quaternion_integrator/quaternion_integrator_multi_bodies.py
@@ -29,7 +29,10 @@ class QuaternionIntegrator(object):
     self.linear_operator = None
     self.eta = None
     self.a = None
-   
+    self.velocities = None
+    self.velocities_previous_step = None
+    self.first_step = True
+
     # Optional variables
     self.calc_slip = None
     self.calc_force_torque = None
@@ -190,9 +193,24 @@ class QuaternionIntegrator(object):
     where v_i and w_i are the linear and angular velocities of body i.
     ''' 
     while True: 
-      # Calculate slip on blobs    
-      print 'deterministic_adams_bashforth ---------------------'
-      
+      # Solve mobility problem
+      velocities = self.solve_mobility_problem()
+
+      # Update location and orientation
+      if self.first_step == True:
+        # Use forward Euler
+        for k, b in enumerate(self.bodies):
+          b.location += velocities[6*k:6*k+3] * dt
+          quaternion_dt = Quaternion.from_rotation((velocities[6*k+3:6*k+6]) * dt)
+          b.orientation = quaternion_dt * b.orientation      
+      else:
+        # Use Adams-Bashforth
+        for k, b in enumerate(self.bodies):
+          b.location += (1.5 * velocities[6*k:6*k+3] - 0.5 * self.velocities_previous_step[6*k:6*k+3]) * dt
+          quaternion_dt = Quaternion.from_rotation((1.5 * velocities[6*k+3:6*k+6] \
+                                                      - 0.5 * self.velocities_previous_step[6*k+3:6*k+6]) * dt)
+          b.orientation = quaternion_dt * b.orientation
+
       # Check positions, if valid return 
       valid_configuration = True
       for b in self.bodies:
@@ -200,10 +218,70 @@ class QuaternionIntegrator(object):
         if valid_configuration is False:
           break
       if valid_configuration is True:
+        # Save velocities for next step
+        self.first_step = False
+        self.velocities_previous_step = velocities
         return
     
     print 'Invalid configuration'      
     return
+
+
+  def solve_mobility_problem(self): 
+    ''' 
+    Solve the mobility problem using preconditioned GMRES. Compute 
+    velocities on the bodies subject to active slip and enternal 
+    forces-torques.
+
+    The linear and angular velocities are sorted lile
+    velocities = (v_1, w_1, v_2, w_2, ...)
+    where v_i and w_i are the linear and angular velocities of body i.
+    ''' 
+    while True: 
+      # Calculate slip on blobs
+      if self.calc_slip is not None:
+        slip = self.calc_slip(self.bodies, self.Nblobs)
+      else:
+        slip = np.zeros((self.Nblobs, 3))
+
+      # Get blobs coordinates
+      r_vectors_blobs = self.get_blobs_r_vectors(self.bodies, self.Nblobs)
+
+      # Calculate force-torque on bodies
+      force_torque = self.force_torque_calculator(self.bodies, r_vectors_blobs)
+
+      # Set right hand side
+      System_size = self.Nblobs * 3 + len(self.bodies) * 6
+      RHS = np.reshape(np.concatenate([slip, -force_torque]), (System_size))
+
+      # Set linear operators 
+      linear_operator_partial = partial(self.linear_operator, bodies=self.bodies, r_vectors=r_vectors_blobs, eta=self.eta, a=self.a)
+      A = spla.LinearOperator((System_size, System_size), matvec = linear_operator_partial, dtype='float64')
+
+      # Set preconditioner
+      self.mobility_blobs_cholesky = []
+      # 1. Loop over bodies
+      for k, b in enumerate(self.bodies):
+        # 2. Compute body mobility
+        N = b.calc_mobility_body(self.eta, self.a)
+        self.mobility_bodies[k] = N
+        # 3. Compute cholesky factorization
+        L = np.empty((3*b.Nblobs, 3*b.Nblobs))
+        L = b.calc_mobility_blobs_cholesky(self.eta, self.a)
+        self.mobility_blobs_cholesky.append(L)
+      # 4. Pack preconditioner
+      PC_partial = partial(self.preconditioner, bodies=self.bodies, mobility_bodies=self.mobility_bodies, \
+                             mobility_blobs_cholesky=self.mobility_blobs_cholesky, Nblobs=self.Nblobs)
+      PC = spla.LinearOperator((System_size, System_size), matvec = PC_partial, dtype='float64')
+      # PC = None
+
+      # Solve preconditioned linear system
+      (sol_precond, info_precond) = spla.gmres(A, RHS, x0=self.first_guess, tol=1e-8, M=PC, maxiter=1000, restart=60, callback=make_callback())
+      self.first_guess = sol_precond  
+
+      # Extract velocities
+      return np.reshape(sol_precond[3*self.Nblobs: 3*self.Nblobs + 6*len(self.bodies)], (len(self.bodies) * 6))
+
 
 # Callback generator
 def make_callback():
