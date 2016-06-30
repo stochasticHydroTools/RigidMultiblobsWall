24adebf3f27f19e01b0801fa719d9b2d6da1f943
Patrick Mooney <pmm4k3@mail.missouri.edu>
2016-06-30 19:16:51 -0400

    added the GB distribution

--- single_sphere/sphere.py
+++ single_sphere/sphere.py
@@ -16,12 +16,15 @@ KT = 300.*1.3806488e-5  # T = 300K
 # these were made up somewhat arbitrarily
 REPULSION_STRENGTH = 7.5*KT
 DEBYE_LENGTH = 0.5*A
+# formula for max_height chosen by Stephen. Works well enough, though I'm not sure why
+max_height = KT/weight*12 + A + 4.*DEBYE_LENGTH
+
 
 
 # return the boltzmann distribution for one sphere
 # the potential energy need only be composed of the gravitational potential
 # energy and the energy from the wall repulsion
-def single_sphere_generate_boltzmann_distribution(location, new_location):
+def single_sphere_generate_boltzmann_distribution(new_location):
 	U = 0
 	h = new_location[2]
 	U += weight * h
@@ -34,14 +37,11 @@ def single_sphere_generate_boltzmann_distribution(location, new_location):
 # probability is formed using single_sphere_generate_boltzmann_distribution.
 # a random value between 0 and 1 is compared to the acceptance probability
 # and so a reasonable height is returned
-def single_sphere_generate_equilibrium_sample_rejection(location, partitionZ):
-	# formula for max_height chosen by Stephen. Works well enough, though I'm not sure why
-	max_height = KT/weight*12 + A + 4.*DEBYE_LENGTH
-
+def single_sphere_generate_equilibrium_sample_rejection(partitionZ):
 	# generate heights and their corresponding probabilities until a height passes unrejected
 	while True:
 		new_location = [0., 0., np.random.uniform(A, max_height-A)]
-		acceptance_prob = (single_sphere_generate_boltzmann_distribution(location,new_location))/partitionZ
+		acceptance_prob = (single_sphere_generate_boltzmann_distribution(new_location))/partitionZ
 		
 		if np.random.uniform(0., 1.) < acceptance_prob: # the rejection part of the algorithm. 
 			return new_location
@@ -51,26 +51,58 @@ def single_sphere_generate_equilibrium_sample_rejection(location, partitionZ):
 # constant to use in single_sphere_generate_equilibrium_sample_rejection
 # the function uses the maximum sample for the partition function and multiplies by the 
 # constant 2 in order to ensure that the acceptance probability is below 1, always
-def generate_partition_function(location):
-	max_height = KT/weight*12 + A + 4.*DEBYE_LENGTH
+def generate_partition_function():
 	partitionZ = 0
 	#for i in range(100):
 	#	new_location = [0., 0., np.random.uniform(A, max_height-A)]
 	#	partitionZ += single_sphere_generate_boltzmann_distribution(location,new_location)
 	for i in range(10000):
 		new_location = [0., 0., np.random.uniform(A, max_height-A)]
-		sample = single_sphere_generate_boltzmann_distribution(location,new_location)
+		sample = single_sphere_generate_boltzmann_distribution(new_location)
 		if sample > partitionZ:
 			partitionZ = sample
 	return partitionZ*2
 
 
 # generate the histogram of the heights by reading in the heights from the given file to x
-def plot_distribution(locationsFile):
-	x = np.loadtxt(locationsFile, float)
-	plt.hist(x, 500, normed=1, facecolor='green', alpha=0.75)
-	plt.title('Probability distribution of the height z of a single sphere near a wall')
+# and plot the analytical distribution curve given by x and y
+def plot_distribution(locationsFile, x, y):
+	heights = np.loadtxt(locationsFile, float)
+	plt.hist(heights, 500, normed=1, facecolor='green', alpha=0.75)
+	plt.plot(x,y,'b-', linewidth=1.5)
+	plt.title('Probability distribution of the height z of a single sphere near a wall\n' + 
+			  'Green: histogram of sampled heights  Blue: GB distribution')
 	plt.xlabel('z (microns)')
 	plt.ylabel('P(z)')
 	plt.axis([0, 12.5, 0, .35])
 	plt.show()
+
+
+# calculate 100,000 points given by directly solving the Gibbs-Boltzmann distribution
+# P(h) = exp(-U(h)/KT) / integral(exp(U(h)/KT)dh)
+# calculated using the trapezoidal rule
+def analytical_distribution():
+	# 100,000 heights are sampled evenly from the chosen bounds, 1.5 is somewhat arbitrary
+	# 0 is not chosen because I was getting an overflow in potential energy U when the
+	# sphere was very close to the wall
+	x = np.linspace(1.5, max_height, 100000) 
+	y = []
+	deltaX = x[1] - x[0] 
+	numerator, denominator = 0., 0.
+
+	# add the bounds to the integral value
+	integral = 0.5*(single_sphere_generate_boltzmann_distribution([0., 0., 1.5]) + 
+					single_sphere_generate_boltzmann_distribution([0., 0., max_height]))
+	# iterate over the rest of the heights
+	for k in np.linspace(x[1], x[99998], 99998):
+		integral += single_sphere_generate_boltzmann_distribution([0., 0., k])
+	# multiply by the change in x to complete the integral calculation
+	integral *= deltaX
+
+	# now that we have the partition function that the integral represents
+	# we can calculate all the y positions of the distribution 
+	for h in x:
+		numerator = single_sphere_generate_boltzmann_distribution([0., 0., h])		
+		y.append(numerator/integral)
+	
+	return x, y
