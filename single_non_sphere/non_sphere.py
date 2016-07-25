d12064363fe92d18dd413f993c1f282057d0b787
Patrick Mooney <pmm4k3@mail.missouri.edu>
2016-07-25 17:10:45 -0400

    changes made before shifting program to general bodies

--- single_non_sphere/non_sphere.py
+++ single_non_sphere/non_sphere.py
@@ -26,20 +26,23 @@ def non_sphere_GB(location, orientation):
 	''' Return exp(-U/kT) for the given location and orientation.'''
 	r_vectors = get_boomerang_r_vectors(location, orientation)
 	# Add gravity to potential.
+	#for k in range(len(r_vectors)):
+	#	if r_vectors[k][2] < A:
+	#		return 0.0
+	U = 0.
 	for k in range(len(r_vectors)):
 		if r_vectors[k][2] < A:
 			return 0.0
-	U = 0
-	for k in range(len(r_vectors)):
-		U += WEIGHT[k]*r_vectors[k][2]
+		U += WEIGHT[k] * r_vectors[k][2]
 		h = r_vectors[k][2]
 		# Add repulsion to potential.
 		U += ( REPULSION_STRENGTH * np.exp(-1.*(h -A)/DEBYE_LENGTH) / (h-A) )
-
+	#U *= REPULSION_STRENGTH
+	#U += np.sum(WEIGHT[0]*r_vectors[:,2])
 	return np.exp(-1. * U/KT)
 
 
-def get_boomerang_r_vectors(location, orientation):
+def get_boomerang_r_vectors(location, orientation, blob_distance = .525):
 	'''Get the vectors of the 7 blobs used to discretize the boomerang.
   
 		 7 O  
@@ -62,20 +65,21 @@ def get_boomerang_r_vectors(location, orientation):
         and then use const in the code. This way you can change it with one-line.
         It is a really really bad idea to hard-code values like this...
 	'''
-	
-	initial_configuration = [np.array([1.575, 0., 0.]),
-							 np.array([1.05, 0., 0.]),
-						 	 np.array([0.525, 0., 0.]),
-						 	 np.array([0., 0., 0.]),
-						 	 np.array([0., 0.525, 0.]),
-						 	 np.array([0., 1.05, 0.]),
-						 	 np.array([0., 1.575, 0.])]
+	num_blobs = 7
+	initial_configuration = [  np.array([3*blob_distance, 0., 0.]),
+							 			np.array([2*blob_distance, 0., 0.]),
+						 	 			np.array([blob_distance, 0., 0.]),
+						 	 			np.array([0., 0., 0.]),
+						 	 			np.array([0., blob_distance, 0.]),
+						 	 			np.array([0., 2*blob_distance, 0.]),
+						 	 			np.array([0., 3*blob_distance, 0.])]
 
 	rotation_matrix = orientation.rotation_matrix()
-	rotated_configuration = []
- 	for vec in initial_configuration:
-		rotated_configuration.append(np.dot(rotation_matrix, vec)
-								 + np.array(location))
+	rotated_configuration = [np.dot(rotation_matrix,vec) + location for vec in initial_configuration]
+	#rotated_configuration = []
+ 	#for vec in initial_configuration:
+	#	rotated_configuration.append(np.dot(rotation_matrix, vec)
+	#							   + location)
 
 	return rotated_configuration
 
@@ -86,30 +90,25 @@ def generate_non_sphere_partition(partition_steps):
 	#for i in range(100):
 	#	new_location = [0., 0., np.random.uniform(A, max_height)]
 	#	partitionZ += non_sphere_GB(location,new_location)
+	orientation = Quaternion([0,0,0,0])
+	new_location = np.array([0., 0., 0.])
 	for i in range(partition_steps):
-		theta = np.random.normal(0., 1., 4)
-		orientation = Quaternion(theta/np.linalg.norm(theta))
-		new_location = [0., 0., np.random.uniform(A, max_height)]
+		orientation.random_orientation()
+		new_location[2] = np.random.uniform(A, max_height)
 		sample = non_sphere_GB(new_location, orientation)
 		if sample > partitionZ:
 			partitionZ = sample
-	return partitionZ*1.1 # Donev: It is better if this returns partitionZ,
-        # and the *caller* multiplies by 1.1 or whatever *they* want to do
-        # This makes the code modular and separated. This code accepts partition_steps and returns an honest value
-        # The caller decides if they need to call again with more steps or multiply by a safety or whatever
-        # In programming this is called "contract" -- an agreement that a routine does something
-        # without the caller neededing to know what goes on inside in detail
-        # You define what arguments go in, what comes out, and what side-effects the routine has, and this is the contract
-        # you can then change the implementation to something more efficient but not change the calling code
+	return partitionZ
 
 
 
 def non_sphere_rejection(partitionZ):
-		# generate heights and their corresponding probabilities until a height passes unrejected
+	# generate heights and their corresponding probabilities until a height passes unrejected
+	orientation = Quaternion([0.,0.,0.,0.])
+	new_location = np.array([0., 0., 0.])
 	while True:
-		theta = np.random.normal(0., 1., 4)
-		orientation = Quaternion(theta/np.linalg.norm(theta))
-		new_location = [0., 0., np.random.uniform(A, max_height-A)]
+		orientation.random_orientation()
+		new_location[2] = np.random.uniform(A, max_height-A)
 		acceptance_prob = non_sphere_GB(new_location, orientation) / partitionZ
 		if acceptance_prob > 1:
 			raise InvalidProbability('Acceptance Probability is greater than 1')
@@ -127,34 +126,75 @@ def analytical_distribution_non_sphere(num_points):
 	# because linspace includes starting value A, the first index in x is ignored
 	# if x[0] is included, then in the calculation of potential energy U, h-A = 0
 	# and an exception will be thrown
-	x = np.linspace(A, max_height, num_points)
-	orientations = [] 
-	for i in range(num_points):
-		theta = np.random.normal(0., 1., 4)
-		orientations.append(Quaternion(theta/np.linalg.norm(theta)))
-	y = []
-	deltaX = x[1] - x[0] 
-	numerator, denominator = 0., 0.
+#	x = np.linspace(A, max_height, num_points)
+#	orientations = [] 
+#	for i in range(num_points):
+#		theta = np.random.normal(0., 1., 4)
+#		orientations.append(Quaternion(theta/np.linalg.norm(theta)))
+#	y = []
+#	deltaX = x[1] - x[0] 
+#	numerator, denominator = 0., 0.
+#
+#	# add the bounds to the integral value
+#	# ignore x[0] = A
+#	integral = 0.5*(non_sphere_GB([0., 0., x[1]], orientations[0]) + 
+#					non_sphere_GB([0., 0., max_height], orientations[num_points-1]))
+#	# iterate over the rest of the heights
+#	for k in range(2, num_points):
+#		integral += non_sphere_GB([0., 0., x[k]], orientations[k])
+#	# multiply by the change in x to complete the integral calculation
+#	integral *= deltaX
+#
+#	# now that we have the partition function that the integral represents
+#	# we can calculate all the y positions of the distribution 
+#	# again ignore x[0] = A
+#	j = 0
+#	for h in x[1:]:
+#		numerator = non_sphere_GB([0., 0., h], orientations[j])		
+#		y.append(numerator/integral)
+#		j+=1
 
-	# add the bounds to the integral value
-	# ignore x[0] = A
-	integral = 0.5*(non_sphere_GB([0., 0., x[1]], orientations[0]) + 
-					non_sphere_GB([0., 0., max_height], orientations[num_points-1]))
-	# iterate over the rest of the heights
-	for k in range(2,num_points):
-		integral += non_sphere_GB([0., 0., x[k]], orientations[k])
-	# multiply by the change in x to complete the integral calculation
+
+	
+	x = np.linspace(A, max_height, num_points)
+	y = np.zeros(num_points-1, dtype = float)
+	num_angles = 1000
+	deltaX = x[1] - x[0]
+	integral = .0
+	firstBar, lastBar = 0., 0.
+	orientation = Quaternion([0,0,0,0]) # create a quaternion object
+
+	for i in range(num_angles):
+		orientation.random_orientation()
+		firstBar += non_sphere_GB([0, 0, x[1]], orientation)
+	firstBar /= num_angles
+
+	for i in range(num_angles):
+		orientation.random_orientation()
+		lastBar += non_sphere_GB([0, 0, x[num_points-1]], orientation)
+	lastBar /= num_angles
+
+	integral += (firstBar + lastBar) *.5
+
+	sample_GB = np.zeros(num_angles, dtype = float)
+	for i in range(2, num_points-1):
+		for j in range(num_angles):
+			orientation.random_orientation()
+			sample_GB[j] = non_sphere_GB(np.array([0, 0, x[i]]), orientation)
+		integral += np.average(sample_GB)
 	integral *= deltaX
 
-	# now that we have the partition function that the integral represents
-	# we can calculate all the y positions of the distribution 
-	# again ignore x[0] = A
-	j = 0
-	for h in x[1:]:
-		numerator = non_sphere_GB([0., 0., h], orientations[j])		
-		y.append(numerator/integral)
-		j+=1
+	for i in range(x[1:].size):
+		numerator = 0.
+		for j in range(num_angles):
+			orientation.random_orientation()
+			numerator += non_sphere_GB([0., 0., x[i+1]], orientation)
+		numerator /= num_angles
+		y[i] = (numerator/integral)
+
+
 	return x[1:], y
+
 
 
 
@@ -181,21 +221,21 @@ def plot_distribution(locationsFile, analytical_x, analytical_y, n_steps):
 		confidence.append( (4 * np.sqrt(numSamples)) / (binWidth * n_steps)) 
 	plt.errorbar(xError,yError,yerr=confidence,fmt='r.')
 
-	p, q = [], []
-	skip = 100
-	size = 100000
-	for i in range(0, size-skip, skip):
-		average = 0.
-		for j in range(i,i+skip):
-			average += analytical_y[j]
-			#print j
-		average /= float(skip)
-		#print("%f   %f" % (analytical_x[i], average))
-		p.append(analytical_x[i])
-		q.append(average)
+#	p, q = [], []
+#	skip = 1000
+#	size = 100000
+#	for i in range(0, size-skip, skip):
+#		average = 0.
+#		for j in range(i,i+skip):
+#			average += analytical_y[j]
+#			#print j
+#		average /= float(skip)
+#		#print("%f   %f" % (analytical_x[i], average))
+#		p.append(analytical_x[i])
+#		q.append(average)
 
-	#plt.plot(analytical_x, analytical_y, 'bo', linewidth=1.5)
-	plt.plot(p, q, 'b-', linewidth=2)
+	plt.plot(analytical_x, analytical_y, 'b.-', linewidth=1.5)
+	#plt.plot(p, q, 'b-', linewidth=2)
 
 	plt.title('Probability distribution of the height z of a single boomerang near a wall\n' + 
 			  'Green: histogram of sampled heights  Blue: GB distribution')
