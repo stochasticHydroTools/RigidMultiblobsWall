df25e72c014601750fe4d5b0da575aec0cfda479
Floren <fbusabiaga@gmail.com>
2018-07-13 11:33:33 -0400

    pass mass options to force

--- multi_bodies/examples/rods/multi_bodies.py
+++ multi_bodies/examples/rods/multi_bodies.py
@@ -684,7 +684,8 @@ if __name__ == '__main__':
                                                debye_length_wall = read.debye_length_wall, 
                                                repulsion_strength = read.repulsion_strength, 
                                                debye_length = read.debye_length, 
-                                               periodic_length = read.periodic_length) 
+                                               periodic_length = read.periodic_length,
+                                               mass_options = read.mass_options) 
   integrator.calc_K_matrix_bodies = calc_K_matrix_bodies
   integrator.calc_K_matrix = calc_K_matrix
   integrator.linear_operator = linear_operator_rigid
