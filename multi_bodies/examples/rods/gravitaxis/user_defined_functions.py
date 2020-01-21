4e2b2646bf53b2c9065ba8d442cb1a379128cea0
Floren <fbusabiaga@gmail.com>
2020-01-21 17:54:24 -0500

    fix in one blob forces after merge with Robert's changes

--- multi_bodies/examples/rods/gravitaxis/user_defined_functions.py
+++ multi_bodies/examples/rods/gravitaxis/user_defined_functions.py
@@ -23,6 +23,42 @@ import multi_bodies_functions
 from multi_bodies_functions import *
 
 
+def blob_external_forces_new(r_vectors, *args, **kwargs):
+  '''
+  This function compute the external force acting on a
+  single blob. It returns an array with shape (3).
+
+  In this example we add gravity and a repulsion with the wall;
+  the interaction with the wall is derived from the potential
+
+  U(z) = U0 + U0 * (a-z)/b   if z<a
+  U(z) = U0 * exp(-(z-a)/b)  iz z>=a
+
+  with
+  e = repulsion_strength_wall
+  a = blob_radius
+  h = distance to the wall
+  b = debye_length_wall
+  '''
+  N = r_vectors.size // 3
+  f = np.zeros((N, 3))
+
+  # Get parameters from arguments
+  blob_mass = kwargs.get('blob_mass')
+  blob_radius = kwargs.get('blob_radius')
+  repulsion_strength_wall = kwargs.get('repulsion_strength_wall')
+  debye_length_wall = kwargs.get('debye_length_wall')
+
+  # Add wall interaction
+  h = r_vectors[:,2]
+  lr_mask = h > blob_radius
+  sr_mask = h <= blob_radius
+  f[lr_mask,2] += (repulsion_strength_wall / debye_length_wall) * np.exp(-(h[lr_mask]-blob_radius)/debye_length_wall)
+  f[sr_mask,2] += (repulsion_strength_wall / debye_length_wall)
+  return f
+multi_bodies_functions.blob_external_forces = blob_external_forces_new
+
+
 def blob_external_force_new(r_vectors, *args, **kwargs):
   '''
   This function compute the external force acting on a
