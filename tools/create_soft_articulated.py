86e40ed6b139982bab193eaa1658037425586f6d
Florencio Balboa Usabiaga <fbusabiaga@gmail.com>
2020-12-06 20:26:17 +0100

    minor change in utility code

--- tools/create_soft_articulated.py
+++ tools/create_soft_articulated.py
@@ -49,3 +49,16 @@ if __name__ == '__main__':
     f.write(str(len(links)) + '\n')
     for l in links:
       f.write(str(int(l[0])) + ' ' + str(int(l[1])) + ' ' + str(int(l[2])) + ' ' + str(int(l[3])) + ' ' + str(l[4]) + ' ' + str(l[5]) + ' ' + str(l[6]) + ' ' + str(l[7]) + ' ' + str(l[8]) + ' ' + str(l[9]) + '\n')
+
+  # Save clones file
+  with open('create_soft_articulated.clones', 'w') as f:
+    f.write(str(x.size) + '\n')
+    for ri in r:
+      f.write(str(ri[0]) + ' ' + str(ri[1]) + ' ' + str(ri[2]) + ' 1 0 0 0 \n')
+
+  # Save vertex list
+  with open('create_soft_articulated.list_vertex', 'w') as f:
+    for i in range(x.size):
+      f.write('Structures/blob.vertex \n')
+
+    
