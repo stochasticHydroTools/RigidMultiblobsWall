42a10f532ee1be54caa4e1831ac7b2a3c1505711
Florencio Balboa Usabiaga <fbusabiaga@gmail.com>
2021-02-21 17:24:22 +0100

    passing periodic_length in multi_bodies_utilities

--- multi_bodies/multi_bodies_utilities.py
+++ multi_bodies/multi_bodies_utilities.py
@@ -272,7 +272,7 @@ if __name__ ==  '__main__':
       offset += b.Nblobs
     
     # Set linear operators 
-    linear_operator_partial = partial(multi_bodies.linear_operator_rigid, bodies=bodies, r_vectors=r_vectors_blobs, eta=read.eta, a=read.blob_radius)
+    linear_operator_partial = partial(multi_bodies.linear_operator_rigid, bodies=bodies, r_vectors=r_vectors_blobs, eta=read.eta, a=read.blob_radius, periodic_length=read.periodic_length)
     A = spla.LinearOperator((System_size, System_size), matvec = linear_operator_partial, dtype='float64')
 
     # Set preconditioner
