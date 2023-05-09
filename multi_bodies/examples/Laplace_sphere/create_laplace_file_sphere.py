ad763c9f79fb0283473a5757a56c5e3068b0d8e9
Florencio Balboa Usabiaga <fbusabiaga@gmail.com>
2023-05-09 10:45:38 +0200

    use first oder Legendre polynomial to write Laplace file

--- multi_bodies/examples/Laplace_sphere/create_laplace_file_sphere.py
+++ multi_bodies/examples/Laplace_sphere/create_laplace_file_sphere.py
@@ -1,3 +1,7 @@
+'''
+Write Laplace file. 
+We use a first order Legendre polynomial for the reaction rate, emitting rate and surface mobility.
+'''
 from __future__ import division, print_function
 import os
 import numpy as np
@@ -22,13 +26,16 @@ while found_functions is False:
       sys.exit()
 
 # Read vertex file to compute the normals and parameters
-#filename  = "../../Structures/shell_N_12_Rg_0_7921_Rh_1"
+filename  = "../../Structures/shell_N_12_Rg_0_7921_Rh_1"
 #filename  = "../../Structures/shell_N_42_Rg_0_8913_Rh_1"
 #filename = "../../Structures/shell_N_162_Rg_0_9497_Rh_1"
-filename  = "../../Structures/shell_N_642_Rg_0_9767_Rh_1"
-alpha = 0
-k = 0
-surface_mobility = 1
+#filename  = "../../Structures/shell_N_642_Rg_0_9767_Rh_1"
+alpha_0 = 0
+alpha_1 = 0
+k_0 = 0
+k_1 = 0
+surface_mobility_0 = 1
+surface_mobility_1 = 1
 Rweight = 1
 
 # Read file
@@ -36,14 +43,26 @@ struct_ref_config = read_vertex_file.read_vertex_file(filename + '.vertex')
 Nb = struct_ref_config.shape[0]
 Rg = np.linalg.norm(struct_ref_config[0,:])
 
+# Extract coordinates
+x = struct_ref_config[:,0]
+y = struct_ref_config[:,1]
+z = struct_ref_config[:,2]
+
+# Get blobs polar angles
+theta = np.arctan2(np.sqrt(x**2 + y**2), z)
+
 # Compute normals
 normals = struct_ref_config / Rg
+
 # Reaction rates
-k_vec = np.zeros((Nb,1)) * k
+k_vec = np.ones((Nb,1)) * k_0 + np.cos(theta) * k_1
+
 # Emission rates
-alpha_vec = np.ones((Nb,1)) * alpha
+alpha_vec = np.ones((Nb,1)) * alpha_0 + np.cos(theta) * alpha_1
+
 # Surface mobility
-surface_mobility_vec = np.ones((Nb,1)) * surface_mobility
+surface_mobility_vec = np.ones((Nb,1)) * surface_mobility_0 + np.cos(theta) * surface_mobility_1 
+
 # Weights of each DOF based on a radius Rweight
 weights = 4.0 * np.pi * Rweight**2 / Nb * np.ones((Nb,1))
 
