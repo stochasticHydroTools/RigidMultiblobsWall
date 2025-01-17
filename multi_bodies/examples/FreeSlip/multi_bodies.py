60c3be34c27c99b8b59135374b3c08d6c1692051
Florencio Balboa Usabiaga <fbusabiaga@gmail.com>
2025-01-17 15:38:35 +0100

    minor change example

--- multi_bodies/examples/FreeSlip/multi_bodies.py
+++ multi_bodies/examples/FreeSlip/multi_bodies.py
@@ -1477,7 +1477,6 @@ if __name__ == '__main__':
   # Prepare linear operator and other function for slip boundary conditions (bc)
   integrator.slip_mode = True
   if read.mobility_vector_prod_implementation.find('stkfmm') >= 0:
-    no_wall_double_layer = set_double_layer_kernels(read.stkfmm_mult_order, read.stkfmm_pbc, read.stkfmm_max_points, L=read.periodic_length, comm=comm)
     no_wall_double_layer = set_double_layer_kernels('PVelLaplacian',
                                                     read.stkfmm_mult_order,
                                                     read.stkfmm_pbc,
